73a3f8abb100cd2fd0eb81c9e94acf8da9cb55fc
silvamha <silva.email@yahoo.com>
2023-12-16 19:25:24 -0500

    Added Bulma changed classes

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
-document.getElementById("bored-bot").addEventListener("click", getIdea)
+btn = document.querySelector(".button")
+
+btn.addEventListener("click", getIdea)
+
 
 function getIdea() {
     fetch("https://www.boredapi.com/api/activity")
         .then(res => res.json())
         .then(data => {
-            document.body.classList.add("fun")
-            document.getElementById("idea").textContent = data.activity
-            document.getElementById("title").textContent = "🦾 HappyBot🦿"
+            // document.body.classList.add("fun")
+            document.querySelector(".idea").textContent = data.activity
+            document.querySelector(".title-el").textContent = "🦾 HappyBot🦿"
             console.log(`This is the message that is displayed from the API: ${data.activity}`)
         })
 
@@ -14,8 +17,8 @@ function getIdea() {
         .then(res => res.json())
         .then(data => {
             console.log(`When the button is clicked, the API returns the following url: ${data.message}`)
-            document.querySelector('#image-container').innerHTML=`
-            <img src="${data.message}">
+            document.querySelector('.image-container').innerHTML=`
+            <img class ="return-image" src="${data.message}">
             `
         })
 
